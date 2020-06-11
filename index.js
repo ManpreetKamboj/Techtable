1d1a8c6deacec3f27a628b55b330fd36531bc7e1
ManpreetKamboj <43692418+ManpreetKamboj@users.noreply.github.com>
2020-06-10 23:48:52 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Logo from './Capture.PNG';
+import Solid from './solid.PNG';
+import Wave from './wave.PNG';
+import App from './App';
+import {BrowserRouter}from 'react-router-dom';
+
+
+
+ReactDOM.render(
+
+  <BrowserRouter>
+  <App/>
+  </BrowserRouter>
+  
+  ,document.getElementById('root')
+
+
+ 
+  
+);
+
+
